8a7a6134a143bc04d78b523f55f2870b1081ae55
krithikasvraman <krithika.svraman@gmail.com>
2025-04-11 18:23:53 +0530

    updated install script

--- test/install_subtec_dependencies.sh
+++ test/install_subtec_dependencies.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# *****************************************************************************
+#
 # If not stated otherwise in this file or this component's LICENSE file the
 # following copyright and licenses apply:
 #
-# Copyright 2021 Liberty Global Service B.V.
+# Copyright 2021 RDK Management
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,71 +17,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Copyright 2023 Comcast Cable Communications Management, LLC
-# Licensed under the Apache License, Version 2.0
-#*****************************************************************************/
 
-function package_exists_lin_fn() {
-    dpkg -s "$1" &> /dev/null
-    return $?
-}
-
-function install_package_fn() {
-    if ! package_exists_lin_fn $1 ; then
-        echo "Installing $1"
-        sudo apt install $1 -y
-        if [ $? == 0 ] ; then
-            INSTALL_STATUS_ARR+=("$1 was successfully installed.")
-        else
-            INSTALL_STATUS_ARR+=("The package $1 FAILED to be installed.")
-        fi
-    else
-       echo "$1 is already installed."
-       INSTALL_STATUS_ARR+=("$1 is already installed.")
-    fi
-}
-
-function install_pkgs_linux_fn()
-{
-    sudo apt update
-    install_package_fn cmake
-    install_package_fn g++
-    install_package_fn libglib2.0-0
-    install_package_fn libglib2.0-dev
-    install_package_fn libxml2-dev
-    install_package_fn pkg-config
-    install_package_fn freeglut3-dev
-    install_package_fn libboost-all-dev
-    install_package_fn libwebsocketpp-dev
-    install_package_fn libjansson-dev
-    install_package_fn libwayland-dev
-    install_package_fn libxkbcommon-dev
-    install_package_fn libfontconfig-dev
-    install_package_fn libharfbuzz-dev
-    install_package_fn wayland-protocols
-
-    VER=$(grep -oP 'VERSION_ID="\K[\d.]+' /etc/os-release)
-
-    if [ ${VER:0:2} -lt 22 ]; then
-        echo "Please upgrade your Ubuntu version to at least 22:04 LTS. OS version is $VER"
-        return 1
-    fi
-}
-
-function install_pkgs_fn()
-{
-  if [[ "$OSTYPE" == "linux"# ]]; then  
-      install_pkgs_linux_fn
-  fi
-}
-
-if [[ -z "${MAKEFLAGS}" ]]; then
-    export MAKEFLAGS=-j$(nproc)
-fi
-
-# Fail the script should any step fail. To override this behavior use "|| true" on those statements
+# Set -eo pipefail to exit immediately if a pipeline fails
 set -eo pipefail
 
-echo ""
-echo "**# Check/Install dependency packages"
-install_pkgs_fn 
+# Set MAKEFLAGS to use the number of processors
+if [[ -z "${MAKEFLAGS}" ]]; then
+    NPROC=$(nproc)
+    export MAKEFLAGS="-j${NPROC}"
+fi
+
+echo "Check/Install dependency packages"
+
+# Define required packages
+required_packages=(
+    cmake
+    g++
+    libglib2.0-0
+    libglib2.0-dev
+    libxml2-dev
+    pkg-config
+    freeglut3-dev
+    libboost-all-dev
+    libwebsocketpp-dev
+    libjansson-dev
+    libwayland-dev
+    libxkbcommon-dev
+    libfontconfig-dev
+    libharfbuzz-dev
+    wayland-protocols
+)
+
+echo "Checking Ubuntu version..."
+# Check Ubuntu version
+if [ -f "/etc/os-release" ]; then
+    UBUNTU_VERSION=$(grep '^VERSION_ID=' /etc/os-release | sed -E 's/VERSION_ID="?([^"]+)"?/\1/')
+else
+    echo "Error: Could not determine OS version."
+fi
+
+EXPECTED_VERSION="22.04"
+
+if [[ "$UBUNTU_VERSION" != "$EXPECTED_VERSION" ]]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Warning: This script has been designed for Ubuntu ${EXPECTED_VERSION}."
+    echo "Your Ubuntu version is: ${UBUNTU_VERSION}. Please check !"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+fi
+
+# Function to check if a package is installed
+function is_package_installed() {
+    dpkg -s "$1" >/dev/null 2>&1
+}
+
+echo "Installing required packages..."
+sudo apt update
+
+for package in "${required_packages[@]}"; do
+    if ! is_package_installed "$package"; then
+        echo "Installing: $package"
+        sudo apt install -y "$package"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to install package '$package'."
+            exit 1
+        fi
+    else
+        echo "$package is already installed."
+    fi
+done
+
+echo "Dependency setup complete"
+exit 0
+

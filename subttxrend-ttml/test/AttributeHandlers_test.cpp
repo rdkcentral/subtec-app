32a5f2b90455984fcc7d0b24f801a93c0f53c21e
krithikasvraman <krithika.svraman@gmail.com>
2025-07-17 17:24:21 +0530

    RDKEMW-6213 : L1, L2 tests update
    Reason for change: github copilot used to check and add/update tests
    Test Procedure: Run unit tests
    Risks: Low
    Signed-off-by:krithika_venkataraman@comcast.com

--- subttxrend-ttml/test/AttributeHandlers_test.cpp
+++ subttxrend-ttml/test/AttributeHandlers_test.cpp
@@ -26,6 +26,22 @@ class AttributeHandlersTest : public CppUnit::TestFixture
 {
 CPPUNIT_TEST_SUITE( AttributeHandlersTest );
     CPPUNIT_TEST(merging);
+    CPPUNIT_TEST(mergeAttributes_emptySource);
+    CPPUNIT_TEST(mergeAttributes_overwrite);
+    CPPUNIT_TEST(operatorStream_empty);
+    CPPUNIT_TEST(operatorStream_nonEmpty);
+    CPPUNIT_TEST(styleAttributeHandler_addAndGet);
+    CPPUNIT_TEST(styleAttributeHandler_set);
+    CPPUNIT_TEST(testStyleAttributeHandlerAddAttributeOverwrites);
+    CPPUNIT_TEST(testStyleAttributeHandlerSetReplacesAll);
+    CPPUNIT_TEST(testStyleAttributeHandlerGetStyleAttributesReturnsReference);
+    CPPUNIT_TEST(testMergeAttributesEmptyDestination);
+    CPPUNIT_TEST(testOperatorStreamSingleAttribute);
+    CPPUNIT_TEST(testOperatorStreamSpecialChars);
+    CPPUNIT_TEST(testStyleAttributeHandlerEmptyKeyValue);
+    CPPUNIT_TEST(testStyleAttributeHandlerSpecialChars);
+    CPPUNIT_TEST(testStyleAttributeHandlerSetEmpty);
+    CPPUNIT_TEST(testStyleAttributeHandlerSetSelfAssign);
 CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -99,6 +115,161 @@ public:
         auto search_04 = dest.find("fontFamily");
         CPPUNIT_ASSERT(search_04 != dest.end());
         CPPUNIT_ASSERT(search_04->second == someFontName_04);
+    }
+
+    void mergeAttributes_emptySource()
+    {
+        Attributes dest = { {"a", "1"}, {"b", "2"} };
+        Attributes src = {};
+        mergeAttributes(dest, src);
+        CPPUNIT_ASSERT(dest.size() == 2);
+        CPPUNIT_ASSERT(dest.at("a") == "1");
+        CPPUNIT_ASSERT(dest.at("b") == "2");
+    }
+
+    void mergeAttributes_overwrite()
+    {
+        Attributes dest = { {"a", "1"}, {"b", "2"} };
+        Attributes src = { {"a", "3"}, {"c", "4"} };
+        mergeAttributes(dest, src);
+        CPPUNIT_ASSERT(dest.size() == 3);
+        CPPUNIT_ASSERT(dest.at("a") == "3");
+        CPPUNIT_ASSERT(dest.at("b") == "2");
+        CPPUNIT_ASSERT(dest.at("c") == "4");
+    }
+
+    void operatorStream_empty()
+    {
+        Attributes attrSet = {};
+        std::ostringstream oss;
+        oss << attrSet;
+        CPPUNIT_ASSERT(oss.str() == "");
+    }
+
+    void operatorStream_nonEmpty()
+    {
+        Attributes attrSet = { {"x", "10"}, {"y", "20"} };
+        std::ostringstream oss;
+        oss << attrSet;
+        std::string out = oss.str();
+        CPPUNIT_ASSERT(out.find("[x:10], ") != std::string::npos);
+        CPPUNIT_ASSERT(out.find("[y:20], ") != std::string::npos);
+    }
+
+    void styleAttributeHandler_addAndGet()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("foo", "bar");
+        handler.addAttribute("foo", "baz");
+        const Attributes& attrs = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(attrs.size() == 1);
+        CPPUNIT_ASSERT(attrs.at("foo") == "baz");
+    }
+
+    void styleAttributeHandler_set()
+    {
+        StyleAttributeHandler handler;
+        Attributes attrs = { {"a", "1"}, {"b", "2"} };
+        handler.set(attrs);
+        const Attributes& got = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(got.size() == 2);
+        CPPUNIT_ASSERT(got.at("a") == "1");
+        CPPUNIT_ASSERT(got.at("b") == "2");
+    }
+
+    // --- StyleAttributeHandler ---
+    void testStyleAttributeHandlerAddAttributeOverwrites()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("color", "red");
+        handler.addAttribute("color", "blue");
+        CPPUNIT_ASSERT(handler.getStyleAttributes().at("color") == "blue");
+    }
+
+    void testStyleAttributeHandlerSetReplacesAll()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("color", "red");
+        Attributes newAttrs = {{"font", "Arial"}};
+        handler.set(newAttrs);
+        CPPUNIT_ASSERT(handler.getStyleAttributes().size() == 1);
+        CPPUNIT_ASSERT(handler.getStyleAttributes().at("font") == "Arial");
+    }
+
+    void testStyleAttributeHandlerGetStyleAttributesReturnsReference()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("color", "red");
+        const Attributes& ref = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(ref.at("color") == "red");
+    }
+
+    void testMergeAttributesEmptyDestination()
+    {
+        Attributes dest;
+        Attributes src = { {"key1", "value1"}, {"key2", "value2"} };
+        mergeAttributes(dest, src);
+        CPPUNIT_ASSERT(dest.size() == 2);
+        CPPUNIT_ASSERT(dest.at("key1") == "value1");
+        CPPUNIT_ASSERT(dest.at("key2") == "value2");
+    }
+
+    void testOperatorStreamSingleAttribute()
+    {
+        Attributes attrSet = { {"single", "value"} };
+        std::ostringstream oss;
+        oss << attrSet;
+        CPPUNIT_ASSERT(oss.str() == "[single:value], ");
+    }
+
+    void testOperatorStreamSpecialChars()
+    {
+        Attributes attrSet = { {"key:with:colon", "value,with,comma"}, {"key[bracket]", "value]bracket["} };
+        std::ostringstream oss;
+        oss << attrSet;
+        std::string out = oss.str();
+        CPPUNIT_ASSERT(out.find("[key:with:colon:value,with,comma], ") != std::string::npos);
+        CPPUNIT_ASSERT(out.find("[key[bracket]:value]bracket[], ") != std::string::npos);
+    }
+
+    void testStyleAttributeHandlerEmptyKeyValue()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("", "value");
+        handler.addAttribute("key", "");
+        const Attributes& attrs = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(attrs.at("") == "value");
+        CPPUNIT_ASSERT(attrs.at("key") == "");
+    }
+
+    void testStyleAttributeHandlerSpecialChars()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("key:with:colon", "value,with,comma");
+        handler.addAttribute("key[bracket]", "value]bracket[");
+        const Attributes& attrs = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(attrs.at("key:with:colon") == "value,with,comma");
+        CPPUNIT_ASSERT(attrs.at("key[bracket]") == "value]bracket[");
+    }
+
+    void testStyleAttributeHandlerSetEmpty()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("key", "value");
+        Attributes emptyAttrs;
+        handler.set(emptyAttrs);
+        CPPUNIT_ASSERT(handler.getStyleAttributes().empty());
+    }
+
+    void testStyleAttributeHandlerSetSelfAssign()
+    {
+        StyleAttributeHandler handler;
+        handler.addAttribute("key", "value");
+        Attributes& current = const_cast<Attributes&>(handler.getStyleAttributes());
+        handler.set(current);
+        const Attributes& result = handler.getStyleAttributes();
+        CPPUNIT_ASSERT(result.size() == 1);
+        CPPUNIT_ASSERT(result.at("key") == "value");
     }
 
 };
--- subttxrend-ttml/test/Elements_test.cpp
+++ subttxrend-ttml/test/Elements_test.cpp
@@ -26,6 +26,40 @@ class ElementsTest : public CppUnit::TestFixture
 {
 CPPUNIT_TEST_SUITE( ElementsTest );
     CPPUNIT_TEST(defaultStyle);
+    CPPUNIT_TEST(testElementParseAttributeSetsIdAndDerived);
+    CPPUNIT_TEST(testElementExpectTextContentDefault);
+    CPPUNIT_TEST(testStyleElementHandlerRegistration);
+    CPPUNIT_TEST(testRegionElementDefaultConstructor);
+    CPPUNIT_TEST(testRegionElementParameterizedConstructor);
+    CPPUNIT_TEST(testRegionElementAddAttributeOriginExtent);
+    CPPUNIT_TEST(testRegionElementAddAttributeDelegates);
+    CPPUNIT_TEST(testRegionElementGetStyleId);
+    CPPUNIT_TEST(testImageElementExpectTextContent);
+    CPPUNIT_TEST(testImageElementAppendText);
+    CPPUNIT_TEST(testImageElementIsSameImage);
+    CPPUNIT_TEST(testBodyElementDefaultConstructor);
+    CPPUNIT_TEST(testBodyElementParentConstructor);
+    CPPUNIT_TEST(testBodyElementExpectTextContent);
+    CPPUNIT_TEST(testBodyElementAppendText);
+    CPPUNIT_TEST(testBodyElementAppendNewline);
+    CPPUNIT_TEST(testBodyElementGetters);
+    CPPUNIT_TEST(testBodyElementHasValidContent);
+    CPPUNIT_TEST(testBodyElementFinalizeRemovesEmptyLines);
+    CPPUNIT_TEST(testBodyElementParseAttributeDerived);
+    CPPUNIT_TEST(testTTElementDefaultConstructor);
+    CPPUNIT_TEST(testStyleElementMergeAttributes);
+    CPPUNIT_TEST(testRegionElementParseOriginExtentPercent);
+    CPPUNIT_TEST(testRegionElementParseOriginExtentPixels);
+    CPPUNIT_TEST(testRegionElementParseOriginExtentInvalid);
+    CPPUNIT_TEST(testImageElementAppendMultipleChunks);
+    CPPUNIT_TEST(testImageElementIsSameImageDifferentData);
+    CPPUNIT_TEST(testBodyElementWhitespaceHandlingDefault);
+    CPPUNIT_TEST(testBodyElementWhitespaceHandlingPreserve);
+    CPPUNIT_TEST(testRegionElementToStr);
+    CPPUNIT_TEST(testRegionElementAddAttributeInvalid);
+    CPPUNIT_TEST(testBodyElementFinalizeAllLinesEmpty);
+    CPPUNIT_TEST(testBodyElementParseAttributeDerivedUnknown);
+    CPPUNIT_TEST(testTTElementParseAttributeDerivedCellResolutionInvalid);
 CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -46,8 +80,343 @@ public:
 
         CPPUNIT_ASSERT(style.getId() == "style1");
     }
-};
 
+    // --- Element base class ---
+    void testElementParseAttributeSetsIdAndDerived()
+    {
+        class TestElement : public Element {
+        public:
+            bool derivedCalled = false;
+            void parseAttributeDerived(const std::string& name, const std::string& value) override {
+                derivedCalled = true;
+            }
+        };
+        TestElement elem;
+        elem.parseAttribute("", "id", "foo");
+        CPPUNIT_ASSERT_EQUAL(std::string("foo"), elem.getId());
+        elem.parseAttribute("", "other", "bar");
+        CPPUNIT_ASSERT(elem.derivedCalled);
+    }
+
+    void testElementExpectTextContentDefault()
+    {
+        Element* elem = new StyleElement();
+        CPPUNIT_ASSERT(!elem->expectTextContent());
+        delete elem;
+    }
+
+    // --- StyleElement ---
+    void testStyleElementHandlerRegistration()
+    {
+        StyleElement style;
+        style.parseAttribute("tts", "color", "red");
+        CPPUNIT_ASSERT(style.getStyleAttributes().at("color") == "red");
+    }
+
+    void testStyleElementMergeAttributes()
+    {
+        StyleElement style;
+        style.parseAttribute("tts", "color", "red");
+        style.parseAttribute("tts", "font", "Arial");
+        CPPUNIT_ASSERT(style.getStyleAttributes().at("color") == "red");
+        CPPUNIT_ASSERT(style.getStyleAttributes().at("font") == "Arial");
+    }
+    // --- RegionElement ---
+    void testRegionElementDefaultConstructor()
+    {
+        RegionElement region;
+        CPPUNIT_ASSERT(region.getX().getValue() == 0);
+        CPPUNIT_ASSERT(region.getY().getValue() == 0);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 10000);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 10000);
+    }
+
+    void testRegionElementParameterizedConstructor()
+    {
+        RegionElement region(10, 20, 30, 40);
+        CPPUNIT_ASSERT(region.getX().getValue() == 10);
+        CPPUNIT_ASSERT(region.getY().getValue() == 20);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 30);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 40);
+    }
+
+    void testRegionElementAddAttributeOriginExtent()
+    {
+        RegionElement region;
+        region.addAttribute("origin", "5% 10%");
+        region.addAttribute("extent", "20% 30%");
+        CPPUNIT_ASSERT(region.getX().getValue() == 500);
+        CPPUNIT_ASSERT(region.getY().getValue() == 1000);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 2000);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 3000);
+    }
+
+    void testRegionElementAddAttributeDelegates()
+    {
+        RegionElement region;
+        region.addAttribute("color", "blue");
+        CPPUNIT_ASSERT(region.getStyleAttributes().at("color") == "blue");
+    }
+
+    void testRegionElementGetStyleId()
+    {
+        RegionElement region;
+        region.parseAttribute("", "style", "myStyle");
+        CPPUNIT_ASSERT(region.getStyleId() == "myStyle");
+    }
+
+    void testRegionElementParseOriginExtentPercent()
+    {
+        RegionElement region;
+        region.addAttribute("origin", "12.5% 87.5%");
+        region.addAttribute("extent", "50% 25%");
+        CPPUNIT_ASSERT(region.getX().getValue() == 1250);
+        CPPUNIT_ASSERT(region.getY().getValue() == 8750);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 5000);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 2500);
+    }
+
+    void testRegionElementParseOriginExtentPixels()
+    {
+        RegionElement region;
+        region.addAttribute("origin", "100px 200px");
+        region.addAttribute("extent", "300px 400px");
+        CPPUNIT_ASSERT(region.getX().getValue() == 100);
+        CPPUNIT_ASSERT(region.getY().getValue() == 200);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 300);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 400);
+    }
+
+    void testRegionElementParseOriginExtentInvalid()
+    {
+        RegionElement region;
+        region.addAttribute("origin", "bad bad");
+        region.addAttribute("extent", "bad bad");
+        CPPUNIT_ASSERT(region.getX().getValue() == 0);
+        CPPUNIT_ASSERT(region.getY().getValue() == 0);
+        CPPUNIT_ASSERT(region.getWidth().getValue() == 0);
+        CPPUNIT_ASSERT(region.getHeight().getValue() == 0);
+    }
+    // --- ImageElement ---
+    void testImageElementExpectTextContent()
+    {
+        ImageElement img;
+        CPPUNIT_ASSERT(img.expectTextContent());
+    }
+
+    void testImageElementAppendText()
+    {
+        ImageElement img;
+        img.appendText("abc");
+        CPPUNIT_ASSERT(*(img.getBase64Data()) == "abc");
+    }
+
+    void testImageElementIsSameImage()
+    {
+        auto img1 = std::make_shared<ImageElement>();
+        auto img2 = std::make_shared<ImageElement>();
+        img1->parseAttribute("", "id", "imgA");
+        img2->parseAttribute("", "id", "imgA");
+        img1->appendText("data");
+        img2->appendText("data");
+        CPPUNIT_ASSERT(img1->isSameImage(img2));
+    }
+
+    void testImageElementAppendMultipleChunks()
+    {
+        ImageElement img;
+        img.appendText("abc");
+        img.appendText("def");
+        CPPUNIT_ASSERT(*(img.getBase64Data()) == "abcdef");
+    }
+
+    void testImageElementIsSameImageDifferentData()
+    {
+        auto img1 = std::make_shared<ImageElement>();
+        auto img2 = std::make_shared<ImageElement>();
+        img1->parseAttribute("", "id", "imgA");
+        img2->parseAttribute("", "id", "imgA");
+        img1->appendText("data1");
+        img2->appendText("data2");
+        CPPUNIT_ASSERT(!img1->isSameImage(img2));
+    }
+
+    // --- BodyElement ---
+    void testBodyElementDefaultConstructor()
+    {
+        BodyElement body;
+        CPPUNIT_ASSERT(body.getWhiteSpaceHandling() == XmlSpace::DEFAULT);
+        CPPUNIT_ASSERT(body.getStyleId().empty());
+        CPPUNIT_ASSERT(body.getRegionId().empty());
+        CPPUNIT_ASSERT(body.getTextLines().size() == 1);
+    }
+
+    void testBodyElementParentConstructor()
+    {
+        auto parent = std::make_shared<BodyElement>();
+        parent->parseAttribute("", "style", "parentStyle");
+        BodyElement child(parent);
+        CPPUNIT_ASSERT(child.getStyleId() == "parentStyle");
+    }
+
+    void testBodyElementExpectTextContent()
+    {
+        BodyElement body;
+        CPPUNIT_ASSERT(body.expectTextContent());
+    }
+
+    void testBodyElementAppendText()
+    {
+        BodyElement body;
+        body.appendText("hello");
+        CPPUNIT_ASSERT(body.getTextLines().back().text == "hello");
+    }
+
+    void testBodyElementAppendNewline()
+    {
+        BodyElement body;
+        body.appendText("hello");
+        body.appendNewline();
+        CPPUNIT_ASSERT(body.getTextLines().size() == 2);
+        CPPUNIT_ASSERT(body.getTextLines().back().isForcedLine);
+    }
+
+    void testBodyElementGetters()
+    {
+        BodyElement body;
+        body.parseAttribute("", "style", "s1");
+        body.parseAttribute("", "region", "r1");
+        body.parseAttribute("", "backgroundImage", "#img1");
+        CPPUNIT_ASSERT(body.getStyleId() == "s1");
+        CPPUNIT_ASSERT(body.getRegionId() == "r1");
+        CPPUNIT_ASSERT(body.getBackgroundImageId() == "img1");
+    }
+
+    void testBodyElementHasValidContent()
+    {
+        BodyElement body;
+        CPPUNIT_ASSERT(body.hasValidContent());
+        body.appendNewline();
+        CPPUNIT_ASSERT(body.hasValidContent());
+    }
+
+    void testBodyElementFinalizeRemovesEmptyLines()
+    {
+        BodyElement body;
+        body.appendText("");
+        body.appendNewline();
+        body.appendText("abc");
+        body.finalize();
+        for (const auto& line : body.getTextLines()) {
+            CPPUNIT_ASSERT(!line.text.empty() || line.isForcedLine);
+        }
+    }
+
+    void testBodyElementParseAttributeDerived()
+    {
+        BodyElement body;
+        body.parseAttribute("", "begin", "01:02:03.004");
+        body.parseAttribute("", "end", "01:02:04.005");
+        body.parseAttribute("", "space", "preserve");
+        body.parseAttribute("", "style", "s1");
+        body.parseAttribute("", "region", "r1");
+        body.parseAttribute("", "backgroundImage", "#img1");
+        CPPUNIT_ASSERT(body.getWhiteSpaceHandling() == XmlSpace::PRESERVE);
+        CPPUNIT_ASSERT(body.getStyleId() == "s1");
+        CPPUNIT_ASSERT(body.getRegionId() == "r1");
+        CPPUNIT_ASSERT(body.getBackgroundImageId() == "img1");
+    }
+    void testBodyElementWhitespaceHandlingDefault()
+    {
+        BodyElement body;
+        body.appendText("a   b");
+        body.finalize();
+        // Should collapse spaces if XmlSpace::DEFAULT
+        CPPUNIT_ASSERT(body.getTextLines().back().text.find("  ") == std::string::npos);
+    }
+
+    void testBodyElementWhitespaceHandlingPreserve()
+    {
+        BodyElement body;
+        body.parseAttribute("", "space", "preserve");
+        body.appendText("a   b");
+        body.finalize();
+        // Should preserve spaces
+        CPPUNIT_ASSERT(body.getTextLines().back().text.find("  ") != std::string::npos);
+    }
+    // --- TTElement ---
+    void testTTElementDefaultConstructor()
+    {
+        TTElement tt;
+        CPPUNIT_ASSERT(tt.getCellResolution().m_w == 32);
+        CPPUNIT_ASSERT(tt.getCellResolution().m_h == 15);
+    }
+
+    void testTTElementParseAttributeDerivedCellResolution()
+    {
+        TTElement tt;
+        tt.parseAttribute("", "cellResolution", "41 24");
+        CPPUNIT_ASSERT(tt.getCellResolution().m_w == 41);
+        CPPUNIT_ASSERT(tt.getCellResolution().m_h == 24);
+    }
+
+    void testTTElementParseAttributeDerivedCellResolutionZero()
+    {
+        TTElement tt;
+        tt.parseAttribute("", "cellResolution", "0 0");
+        CPPUNIT_ASSERT(tt.getCellResolution().m_w == 32);
+        CPPUNIT_ASSERT(tt.getCellResolution().m_h == 15);
+    }
+
+    void testRegionElementToStr()
+    {
+        RegionElement region;
+        region.parseAttribute("", "id", "regionX");
+        region.addAttribute("origin", "1% 2%");
+        region.addAttribute("extent", "3% 4%");
+        std::string str = region.toStr();
+
+        CPPUNIT_ASSERT(str.find("regionX") != std::string::npos);
+        CPPUNIT_ASSERT(str.find("[1%, 2%, 3%, 4%]") != std::string::npos);
+    }
+
+    void testRegionElementAddAttributeInvalid()
+    {
+        RegionElement region;
+        region.addAttribute("unknown", "value");
+        // Should not throw, just ignored or added as style attribute
+        CPPUNIT_ASSERT(region.getStyleAttributes().at("unknown") == "value");
+    }
+
+    void testBodyElementFinalizeAllLinesEmpty()
+    {
+        BodyElement body;
+        body.appendText("");
+        body.appendNewline();
+        body.appendText("");
+        body.finalize();
+        for (const auto& line : body.getTextLines()) {
+            CPPUNIT_ASSERT(!line.text.empty() || line.isForcedLine);
+        }
+    }
+
+    void testBodyElementParseAttributeDerivedUnknown()
+    {
+        BodyElement body;
+        body.parseAttribute("", "unknown", "value");
+        // Should not throw or crash, just ignored
+        CPPUNIT_ASSERT(true);
+    }
+
+    void testTTElementParseAttributeDerivedCellResolutionInvalid()
+    {
+        TTElement tt;
+        tt.parseAttribute("", "cellResolution", "bad value");
+        // Should fall back to default
+        CPPUNIT_ASSERT(tt.getCellResolution().m_w == 32);
+        CPPUNIT_ASSERT(tt.getCellResolution().m_h == 15);
+    }
+};
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION( ElementsTest );
 
